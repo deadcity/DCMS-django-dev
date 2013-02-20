4f46399546c12c9533bca507ea729da92e180f77
Emery (Jarvis) <m.goss792@gmail.com>
2013-02-20 07:31:23 -0600

    Enable static files.

--- DCMS/urls/main.py
+++ DCMS/urls/main.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # import traits
 
@@ -18,3 +19,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+urlpatterns += staticfiles_urlpatterns()
