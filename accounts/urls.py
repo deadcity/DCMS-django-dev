c62060755e1e5b6d1a73e7536c8f8a156bc9fed7
cmho <cmho@localhost>
2013-09-05 13:24:53 -0500

    Implement registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ from accounts import views as account_views
 urlpatterns = patterns('',
     url(r'^login/$',  views.login,  {'template_name': 'accounts/login.html'},  name = 'login'),
     url(r'^logout/$', views.logout, {'template_name': 'accounts/logout.html'}, name = 'logout'),
-    url(r'^register/$', account_views.register, name = 'register')
+    url(r'^register/$', account_views.register, name = 'register'),
+    url(r'^profile/$', account_views.profile, name = 'profile'),
 )
