ea0b4cf1b0937e64d2c53abf8abc510a7e57d8aa
Emery Goss <m.goss792@gmail.com>
2015-05-15 08:02:52 -0500

    Rename 'accounts' app to 'auth'.

---
+++ auth/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, url
+from django.contrib.auth import views
+from auths import views as auth_views
+
+
+urlpatterns = patterns('',
+    url(r'^login/$',  views.login,  {'template_name': 'auth/login.html'},  name = 'login'),
+    url(r'^logout/$', views.logout, {'template_name': 'auth/logout.html'}, name = 'logout'),
+    url(r'^register/$', auth_views.register, name = 'register'),
+    url(r'^profile/$', auth_views.profile, name = 'profile'),
+)
