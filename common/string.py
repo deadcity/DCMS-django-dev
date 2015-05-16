7e1d4c4647645a532bb1bae510a0e8f57e260cb8
Emery Goss <m.goss792@gmail.com>
2015-05-16 11:32:04 -0500

    Remove a bunch of stuff.

--- common/string.py
+++
@@ -1,8 +0,0 @@
-from re import compile
-
-
-_find_pascal = compile(r'((?<=[a-z0-9])[A-Z]|(?!^)[A-Z](?=[a-z]))')
-
-
-def pascal_to_delimited (target, delim = '_'):
-    return _find_pascal.sub(delim + r'\1', target).lower()
